3af0def622c624145e7426244fa06f43e76d5977
unknown <diandraregita14@gmail.com>
2023-12-13 20:00:51 +0700

    Fix submit behaviour

--- script.js
+++ script.js
@@ -2,21 +2,15 @@ let errorElement = document.getElementById('error');
 errorElement.style.display = 'none';
 // Reference to the form element
 const form = document.getElementById('signup-form');
+const submitButton = document.getElementById('submit');
 
 // Event listener for form submission
-form.addEventListener('submit', function (event) {
-  // Prevent the default form submission
-  event.preventDefault();
-
-  // Check if all requirements are fulfilled (add your validation conditions here)
-  const isRequirementsFulfilled = checkRequirements();
-
-  if (isRequirementsFulfilled) {
+submitButton.addEventListener('click', function () {
+  // Check if all requirements are fulfilled
+  if (checkRequirements()) {
     // If requirements are fulfilled, submit the form
+    submitButton.type = 'submit';
     form.submit();
-  } else {
-    // If requirements are not fulfilled, you can show an error message or take appropriate action
-    alert('Please fill in all required fields and ensure password matching.');
   }
 });
 
@@ -34,28 +28,20 @@ function checkPasswords() {
         errorElement.style.display = 'none';
         document.getElementById('password').style.border = '1px solid #ccc';
         document.getElementById('confirm-password').style.border = '1px solid #ccc';
-        document.querySelector('#submit').setAttribute('type', 'submit');
     }
 }
 
-// Function to check requirements (customize this based on your validation criteria)
+// Function to check requirements
 function checkRequirements() {
-  const firstName = document.getElementById('first-name').value;
-  const lastName = document.getElementById('last-name').value;
-  const email = document.getElementById('email').value;
-  const phoneNumber = document.getElementById('phone-number').value;
-  const password = document.getElementById('password').value;
-  const confirmPassword = document.getElementById('confirm-password').value;
+    let password = document.getElementById('password').value;
+    let confirmPassword = document.getElementById('confirm-password').value;
+    const email = document.getElementById('email').value;
 
-  // Add your validation logic here, e.g., check if fields are not empty, passwords match, etc.
-  // Return true if all requirements are fulfilled, otherwise return false
-  return (
-    firstName.trim() !== '' &&
-    lastName.trim() !== '' &&
-    email.trim() !== '' &&
-    phoneNumber.trim() !== '' &&
-    password.trim() !== '' &&
-    confirmPassword.trim() !== '' &&
-    password === confirmPassword
-  );
+    // Return true if all requirements are fulfilled, otherwise return false
+    return (
+        email.trim() !== '' &&
+        password.trim() !== '' &&
+        confirmPassword.trim() !== '' &&
+        password === confirmPassword
+    );
 }
