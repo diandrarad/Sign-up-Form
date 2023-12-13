17496e4a40a15a365221904201166c1c6f3ff989
unknown <diandraregita14@gmail.com>
2023-12-13 19:11:59 +0700

    Add submit attribute

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let errorElement = document.getElementById('error');
 errorElement.style.display = 'none';
+// Reference to the form element
+const form = document.getElementById('signup-form');
+
+// Event listener for form submission
+form.addEventListener('submit', function (event) {
+  // Prevent the default form submission
+  event.preventDefault();
+
+  // Check if all requirements are fulfilled (add your validation conditions here)
+  const isRequirementsFulfilled = checkRequirements();
+
+  if (isRequirementsFulfilled) {
+    // If requirements are fulfilled, submit the form
+    form.submit();
+  } else {
+    // If requirements are not fulfilled, you can show an error message or take appropriate action
+    alert('Please fill in all required fields and ensure password matching.');
+  }
+});
 
 function checkPasswords() {
     let password = document.getElementById('password').value;
@@ -15,5 +34,28 @@ function checkPasswords() {
         errorElement.style.display = 'none';
         document.getElementById('password').style.border = '1px solid #ccc';
         document.getElementById('confirm-password').style.border = '1px solid #ccc';
+        document.querySelector('#submit').setAttribute('type', 'submit');
     }
+}
+
+// Function to check requirements (customize this based on your validation criteria)
+function checkRequirements() {
+  const firstName = document.getElementById('first-name').value;
+  const lastName = document.getElementById('last-name').value;
+  const email = document.getElementById('email').value;
+  const phoneNumber = document.getElementById('phone-number').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.getElementById('confirm-password').value;
+
+  // Add your validation logic here, e.g., check if fields are not empty, passwords match, etc.
+  // Return true if all requirements are fulfilled, otherwise return false
+  return (
+    firstName.trim() !== '' &&
+    lastName.trim() !== '' &&
+    email.trim() !== '' &&
+    phoneNumber.trim() !== '' &&
+    password.trim() !== '' &&
+    confirmPassword.trim() !== '' &&
+    password === confirmPassword
+  );
 }
